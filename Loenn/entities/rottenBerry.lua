12cd2f2a78a299e5c64ee69a6f63406ee631cb0e
flipking78 <flipking0708@gmail.com>
2022-01-20 11:04:31 +0100

    add entity toggle stuff, rotten berry

---
+++ Loenn/entities/rottenBerry.lua
@@ -0,0 +1,16 @@
+local rottenBerry = {}
+
+rottenBerry.name = "CherryHelper/RottenBerry"
+rottenBerry.texture = "collectables/rottenberry/normal00"
+rottenBerry.justification = {0.5, 0.5}
+rottenBerry.placements = {
+    {
+        name = "rottenBerry",
+        data = {
+            checkpointID = -1,
+            order = -1
+        }
+    }
+}
+
+return rottenBerry

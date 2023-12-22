49bfb285a231624ddd09b36646bcd0447c8da8c5
nhruo <nhruo123@gmail.com>
2023-12-22 17:26:47 +0200

    added a reskinnable UI_NRCB placement

--- Loenn/entities/uninterruptedNonReturnKevin.lua
+++ Loenn/entities/uninterruptedNonReturnKevin.lua
@@ -1,6 +1,7 @@
 local drawableNPatch = require("structs.drawable_nine_patch")
 local drawableRectangle = require("structs.drawable_rectangle")
 local drawableSprite = require("structs.drawable_sprite")
+local utils = require("utils")
 
 local axisOptions = {
     Both = "both",
@@ -12,14 +13,52 @@ local uincrbKevin = {}
 
 uincrbKevin.name = "CherryHelper/UI_NRCB"
 uincrbKevin.depth = 0
-uincrbKevin.minimumSize = {24, 24}
+uincrbKevin.minimumSize = { 24, 24 }
 uincrbKevin.fieldInformation = {
     axes = {
         options = axisOptions,
         editable = false
+    },
+    texturePath = {
+        fieldType = "string",
+    },
+    crushParticleColor1 = {
+        fieldType = "color"
+    },
+    crushParticleColor2 = {
+        fieldType = "color"
+    },
+    activateParticleColor1 = {
+        fieldType = "color"
+    },
+    activateParticleColor2 = {
+        fieldType = "color"
+    },
+    fillColor = {
+        fieldType = "color"
     }
 }
-uincrbKevin.placements = {}
+
+uincrbKevin.ignoredFields = { "_name", "_id", "originX", "originY", "reskinnable" }
+
+uincrbKevin.placements = {
+    {
+        name = "reskinnable",
+        data = {
+            reskinnable = true,
+            width = 24,
+            height = 24,
+            axes = axisOptions.Both,
+            chillout = false,
+            spriteDirectory = "objects/uninterruptedNRK",
+            crushParticleColor1 = "ff66e2",
+            crushParticleColor2 = "68fcff",
+            activateParticleColor1 = "5fcde4",
+            activateParticleColor2 = "ffffff",
+            fillColor = "242262",
+        }
+    }
+}
 
 for _, axis in pairs(axisOptions) do
     table.insert(uincrbKevin.placements, {
@@ -48,17 +87,10 @@ for _, axis in pairs(axisOptions) do
 end
 
 local frameTextures = {
-    none = "objects/crushblock/block00",
-    horizontal = "objects/crushblock/block01",
-    vertical = "objects/crushblock/block02",
-    both = "objects/crushblock/block03"
-}
-
-local altFrameTextures = {
-    none = "objects/uninterruptedNRK/block00",
-    horizontal = "objects/uninterruptedNRK/block01",
-    vertical = "objects/uninterruptedNRK/block02",
-    both = "objects/uninterruptedNRK/block03"
+    none = "/block00",
+    horizontal = "/block01",
+    vertical = "/block02",
+    both = "/block03"
 }
 
 local nPatchOptions = {
@@ -66,16 +98,28 @@ local nPatchOptions = {
     borderMode = "repeat"
 }
 
-local kevinColor = {98 / 255, 34 / 255, 43 / 255}
-local smallFaceTexture = "objects/crushblock/idle_face"
-local giantFaceTexture = "objects/crushblock/giant_block00"
-
-local altKevinColor = {36 / 255, 34 / 255, 98 / 255}
-local altSmallFaceTexture = "objects/uninterruptedNRK/idle_face"
-local altGiantFaceTexture = "objects/uninterruptedNRK/giant_block00"
+local smallFaceTexture = "/idle_face"
+local giantFaceTexture = "/giant_block00"
 
 function uincrbKevin.sprite(room, entity)
+    local spriteDirectory = entity.spriteDirectory
     local altTexture = entity.altTexture or false
+    if spriteDirectory == nil then
+        if altTexture then
+            spriteDirectory = "objects/uninterruptedNRK"
+        else
+            spriteDirectory = "objects/crushblock"
+        end
+    end
+
+    local fillColor = { 98 / 255, 34 / 255, 43 / 255 }
+
+    if entity.fillColor ~= nil then
+        local success, r, g, b = utils.parseHexColor(entity.fillColor)
+        fillColor = { r, g, b }
+    elseif altTexture then
+        fillColor = { 36 / 255, 34 / 255, 98 / 255 }
+    end
 
     local x, y = entity.x or 0, entity.y or 0
     local width, height = entity.width or 24, entity.height or 24
@@ -85,22 +129,13 @@ function uincrbKevin.sprite(room, entity)
 
     local giant = height >= 48 and width >= 48 and chillout
 
-    local faceTexture = giant and giantFaceTexture or smallFaceTexture
-    if altTexture then
-        faceTexture = giant and altGiantFaceTexture or altSmallFaceTexture
-    end
+    local faceTexture = spriteDirectory .. (giant and giantFaceTexture or smallFaceTexture)
 
-    local frameTexture = frameTextures[axes] or frameTextures["both"]
-    if altTexture then
-        frameTexture = altFrameTextures[axes] or altFrameTextures["both"]
-    end
+    local frameTexture = spriteDirectory .. frameTextures[axes]
 
     local ninePatch = drawableNPatch.fromTexture(frameTexture, nPatchOptions, x, y, width, height)
 
-    local rectangle = drawableRectangle.fromRectangle("fill", x + 2, y + 2, width - 4, height - 4, kevinColor)
-    if altTexture then
-        rectangle = drawableRectangle.fromRectangle("fill", x + 2, y + 2, width - 4, height - 4, altKevinColor)
-    end
+    local rectangle = drawableRectangle.fromRectangle("fill", x + 2, y + 2, width - 4, height - 4, fillColor)
 
     local faceSprite = drawableSprite.fromTexture(faceTexture, entity)
     faceSprite:addPosition(math.floor(width / 2), math.floor(height / 2))
